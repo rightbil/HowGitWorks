4aa6fdc9b6864fafe6564ddbad8e62f29ba57e7e
rightbil <rightbil@gmail.com>
2020-02-04 07:42:10 -0500

    setting up on master

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This code is added to git for testing purpose on 2/4/2020
+        System.out.println("Echo......");
+        
+    }
+}
